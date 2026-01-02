dc8d5342606ab00ac7bf7fb813d7e8c363a1aa55
Maysam ghaysari <maysamghaysari80@gmail.com>
2026-01-02 14:28:40 -0800

    update page not-found

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,15 +3,31 @@ import Link from "next/link";
 
 const page = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.contents}>
-        <p className={styles.left_number}>4</p>
-        <div className={styles.mug}></div>
-        <p className={styles.right_number}>4</p>
+        <span className={styles.number}>4</span>
+        
+        {/* المان گرافیکی ماگ قهوه */}
+        <div className={styles.mug_wrapper}>
+          <div className={styles.steam}>
+            <span className={styles.s1}></span>
+            <span className={styles.s2}></span>
+            <span className={styles.s3}></span>
+          </div>
+          <div className={styles.mug}>
+            <div className={styles.handle}></div>
+          </div>
+        </div>
+
+        <span className={styles.number}>4</span>
       </div>
+
       <div className={styles.texts}>
-        <p>صفحه مورد نظر یافت نشد :((</p>
-        <Link href="/">برگشت به صفحه اصلی</Link>
+        <h1>اوپس! فنجان خالیست...</h1>
+        <p>صفحه مورد نظر شما تبخیر شده یا اصلاً وجود نداشته است.</p>
+        <Link href="/" className={styles.back_btn}>
+          بازگشت به کافه (صفحه اصلی)
+        </Link>
       </div>
     </div>
   );
