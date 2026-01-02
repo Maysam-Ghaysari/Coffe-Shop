be0d758dfb2fa382ae811862c31d161cdc81001a
Maysam ghaysari <maysamghaysari80@gmail.com>
2026-01-02 13:49:18 -0800

    add blogs.module.css and add navbar , footer in page blogs

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,15 +1,21 @@
 import BlogCard from "@/components/modules/Blogs/BlogCard";
+import Footer from "@/components/modules/footer/Footer";
+import Navbar from "@/components/modules/navbar/Navbar";
+import styles from "@/styles/blogs.module.css"
 import connectToDB from "@/configs/db";
 import BlogsModel from "@/models/Blogs"
+import { authUser } from "@/utils/ServerHelpers";
 const blogs =  async () => {
+      const user = await authUser();
     connectToDB()
 const Blogs = await BlogsModel.find({}).populate().lean()
     return (
         <>
-        <div>
-                 <h1>بلاگ‌ها</h1>
+        <Navbar isLogin={user ? true : false} />
+        <div className={styles.blogs}>
           <BlogCard  blogs={JSON.parse(JSON.stringify(Blogs))}  />
         </div>
+        <Footer/>
         </>
     )
 }
