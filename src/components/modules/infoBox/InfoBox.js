a8d00b1769ce1c78df496545e4c58931f2ec9bd3
Maysam ghaysari <maysamghaysari80@gmail.com>
2026-01-03 11:53:38 -0800

    update p-user

--- src/components/modules/infoBox/InfoBox.js
+++ src/components/modules/infoBox/InfoBox.js
@@ -1,17 +1,18 @@
 import React from "react";
 import styles from "./infoBox.module.css";
 import { IoStatsChart } from "react-icons/io5";
-const Box = ({ title, value }) => {
+
+const Box = ({ title, value, icon }) => {
   return (
-    <>
-      <div className={styles.box}>
-        <span>{value}</span>
-        <div>
-          <p>{title}</p>
-          <IoStatsChart className={styles.box_chart_icon} />
-        </div>
+    <div className={styles.box}>
+      <div className={styles.box_content}>
+        <p className={styles.box_title}>{title}</p>
+        <h3 className={styles.box_value}>{value?.toLocaleString("fa-IR")}</h3>
       </div>
-    </>
+      <div className={styles.icon_wrapper}>
+        {icon ? icon : <IoStatsChart className={styles.default_icon} />}
+      </div>
+    </div>
   );
 };
 
--- src/components/templates/p-user/index/Tickets.js
+++ src/components/templates/p-user/index/Tickets.js
@@ -1,24 +1,33 @@
 import Ticket from "./Ticket";
 import styles from "./tickets.module.css";
 import Link from "next/link";
-import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowLeft, FaTicketAlt } from "react-icons/fa";
 
 const Tickets = ({ tickets }) => {
   return (
-    <div className={styles.content}>
-      <div className={styles.content_details}>
-        <p>تیکت های اخیر</p>
-        <Link href="/p-user/tickets">
-          همه تیکت ها <FaArrowLeft />
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.title_section}>
+          <FaTicketAlt className={styles.icon} />
+          <h3>تیکت‌های اخیر</h3>
+        </div>
+        <Link href="/p-user/tickets" className={styles.link}>
+          همه تیکت‌ها <FaArrowLeft />
         </Link>
       </div>
- 
-{tickets.length == 0 ? tickets.map((ticket) => (
-        <Ticket key={ticket._id} {...ticket} />
-      )) : <p className={styles.empty}>تیکتی ثبت نشده</p> }
-      
-    </div>
 
+      <div className={styles.list}>
+        {tickets.length > 0 ? (
+          tickets.map((ticket) => (
+            <Ticket key={ticket._id} {...ticket} />
+          ))
+        ) : (
+          <div className={styles.empty_wrapper}>
+            <p className={styles.empty}>هنوز هیچ تیکتی ثبت نکرده‌اید.</p>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
