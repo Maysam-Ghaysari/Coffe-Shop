92abad4e086a4b6e3479cfed7f46600d42478c51
Maysam ghaysari <maysamghaysari80@gmail.com>
2026-01-04 11:40:01 -0800

    update p-admin discounts

--- src/components/templates/p-admin/discounts/Table.js
+++ src/components/templates/p-admin/discounts/Table.js
@@ -3,40 +3,36 @@ import styles from "./table.module.css";
 
 function Table({ discounts }) {
   return (
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          <th>شناسه</th>
-          <th>کد</th>
-          <th>درصد</th>
-          <th>حداکثر استفاده</th>
-          <th>دفعات استفاده شده</th>
-          <th>حذف</th>
-        </tr>
-      </thead>
-      <tbody>
-        {discounts.map((discount, index) => (
-          <tr key={discount._id}>
-            <td
-              className={
-                discount.uses === discount.maxUse ? styles.red : styles.green
-              }
-            >
-              {index + 1}
-            </td>
-            <td>{discount.code}</td>
-            <td>{discount.percent}</td>
-            <td>{discount.maxUse}</td>
-            <td>{discount.uses}</td>
-            <td>
-              <button type="button" className={styles.delete_btn}>
-                حذف
-              </button>
-            </td>
+    <div className={styles.table_container}> {/* کانتینر اسکرول اضافه شد */}
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>شناسه</th>
+            <th>کد</th>
+            <th>درصد</th>
+            <th>حداکثر استفاده</th>
+            <th>دفعات استفاده شده</th>
+            <th>حذف</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {discounts.map((discount, index) => (
+            <tr key={discount._id}>
+              <td className={discount.uses === discount.maxUse ? styles.red : styles.green}>
+                {index + 1}
+              </td>
+              <td>{discount.code}</td>
+              <td>{discount.percent}%</td> {/* اضافه کردن علامت درصد برای زیبایی */}
+              <td>{discount.maxUse}</td>
+              <td>{discount.uses}</td>
+              <td>
+                <button type="button" className={styles.delete_btn}>حذف</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
